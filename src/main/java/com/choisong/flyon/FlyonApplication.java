35f20b28df48d1fac6676ea6c2d524a3fa5f23f7
songhaechan <bukak2019@naver.com>
2025-06-14 15:28:56 +0900

    first commit

---
+++ src/main/java/com/choisong/flyon/FlyonApplication.java
@@ -0,0 +1,13 @@
+package com.choisong.flyon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/choisong/flyon/FlyonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choisong.flyon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
