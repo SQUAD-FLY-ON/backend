b4209e8f202c21fa308889c40461fe2c000cac77
songhaechan <bukak2019@naver.com>
2025-08-15 15:03:58 +0900

    docs: 시/도 날씨정보 문서화

--- src/main/java/com/choisong/flyon/weather/controller/WeatherController.java
+++ src/main/java/com/choisong/flyon/weather/controller/WeatherController.java
@@ -2,6 +2,7 @@ package com.choisong.flyon.weather.controller;
 
 import com.choisong.flyon.weather.dto.SidoWeahterResponse;
 import com.choisong.flyon.weather.service.WeatherService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,7 @@ public class WeatherController {
     private final WeatherService weatherService;
 
     @GetMapping
+    @Operation(summary = "지역별 1-10일간 날씨 정보",description = "시/도 문자열을 입력받아 군/구 단위로 날씨를 반환합니다.")
     public SidoWeahterResponse getVilageForecast(
         @RequestParam String sido,
         @RequestParam String tripStart,
